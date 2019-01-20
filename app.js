3227e650b466ea778041d0fc8045588a347eef82
Yosuke Arai <yoarai@microsoft.com>
2019-01-20 23:49:29 +0900

    express removed

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+/*
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello Node World!!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+*/
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+
